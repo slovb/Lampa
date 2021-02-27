f293b5aa363afca5ed896953e0c09790d96b06a2
Rickard Nyman <nyman.rickard@gmail.com>
2021-02-27 14:01:20 +0100

    Initial commit

---
+++ src/main/java/com/slov/lampa/Lampa.java
@@ -0,0 +1,24 @@
+package com.slov.lampa;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.slov.lampa.init.ModBlocks;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Lampa.MODID)
+public class Lampa
+{
+	public static final String MODID = "lampa";
+	
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    public Lampa() {
+    	IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+    	
+    	ModBlocks.BLOCKS.register(bus);
+    }
+}
---
+++ src/main/java/com/slov/lampa/init/ModBlocks.java
@@ -0,0 +1,13 @@
+package com.slov.lampa.init;
+
+import com.slov.lampa.Lampa;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBlocks {
+	
+	public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Lampa.MODID);
+
+}
